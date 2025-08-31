80e14c67cec485aa44f38c81f71d55e2011387f9
aleksamilicev <amilicev92@gmail.com>
2025-08-31 14:19:06 +0200

    Front graph-project + Back od juce
    - Mali problemi sa tailwindom, graph se lepo prikazuje ali ostali tekst podaci su u basic html-u, ne znam zasto
    - Izmenio/dodao sam sledece klase:
    1. HealthStatusController
    2. NotificationEmailSentEntity
    3. NotificationEmailSentRepo
    4. HealthCheckController
    5. HealthMonitoringService/WorkerRole
    6. NotificationService/WorkerRole

---
+++ ServiceDataRepo/Repositories/NotificationEmailSentRepo.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using ServiceDataRepo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceDataRepo.Repositories
+{
+    public class NotificationEmailSentRepo
+    {
+        private CloudStorageAccount _storageAccount;
+        private CloudTable _table;
+
+        public NotificationEmailSentRepo()
+        {
+            _storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("DataConnectionString"));
+            CloudTableClient tableClient = new CloudTableClient(new Uri(_storageAccount.TableEndpoint.AbsoluteUri), _storageAccount.Credentials);
+            _table = tableClient.GetTableReference("NotificationEmailSentTable");
+            _table.CreateIfNotExists();
+        }
+
+        public IQueryable<NotificationEmailSentEntity> RetrieveAllNotificationEmailsSent()
+        {
+            var results = from g in _table.CreateQuery<NotificationEmailSentEntity>()
+                          where g.PartitionKey == "NotificationEmailSent"
+                          select g;
+            return results;
+        }
+
+        public void AddNotificationEmailSent(NotificationEmailSentEntity entity)
+        {
+            TableOperation insertOperation = TableOperation.Insert(entity);
+            _table.Execute(insertOperation);
+        }
+
+        public IQueryable<NotificationEmailSentEntity> GetNotificationsByAnswerId(Guid answerId)
+        {
+            var results = from g in _table.CreateQuery<NotificationEmailSentEntity>()
+                          where g.PartitionKey == "NotificationEmailSent"
+                             && g.AnswerId == answerId
+                          select g;
+            return results;
+        }
+    }
+}
---
+++ StackOverflow_Cloud/StackOverflowService/Controllers/HealthCheckController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace StackOverflowService.Controllers
+{
+    [RoutePrefix("api")]
+    public class HealthCheckController : ApiController
+    {
+        [HttpGet]
+        [Route("HealthCheck")]
+        public IHttpActionResult HealthCheck()
+        {
+            try
+            {
+                // Ovde možeš dodati dodatne provere:
+                // - Da li je baza dostupna
+                // - Da li su potrebni servisi pokretani
+                // - Da li su connection stringovi validni, itd.
+
+                // Za sada vraća jednostavno OK
+                return Ok("OK");
+            }
+            catch
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
